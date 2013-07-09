080a3c0efa327b9ca9ccc7881d8db65dad665218
Chris Kim <cj3kim@gmail.com>
2013-07-09 14:44:54 -0700

    completed file mover program

--- file_mover.rb
+++ file_mover.rb
@@ -2,10 +2,40 @@ require 'FileUtils'
 
 class FileMover
 
-  def move_all_files
-  end
+  class << self
+    def move_all_files
+      puts "Welcome to the file moving program."
+      puts "Please enter the directory of the files you want to move."
+      start_directory = gets.chomp
 
-  def move_file
-  end
+      puts "Please enter the destination directory."
+      destination_directory = gets.chomp
 
+      puts "Thanks! If you're moving multiple files, please enter a pattern in the file name."
+
+      file_pattern = gets.chomp
+      regex = /#{file_pattern}/i
+      files = Dir.entries(start_directory)
+
+      files.each do |file|
+        if regex === file
+          start_file = File.join(start_directory, file)
+          destination_file = File.join(destination_directory, file)
+
+          move_file(start_file, destination_file)
+
+          puts "We moved #{file} from #{start_directory} to #{destination_directory}."
+        end
+      end
+
+      puts "Finished!"
+    end
+
+    def move_file(file, new_file)
+      FileUtils.mv(file, new_file)
+    end
+
+  end
 end
+
+FileMover.move_all_files
